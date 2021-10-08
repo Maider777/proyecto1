09af4cbfd8358b046e0a60bce10f7cbdd77a5071
2daw3 <2daw3@hz114470.H012108.NET>
2021-10-08 09:34:23 +0200

    first commit

---
+++ dist/form.b1603c8d.js
@@ -0,0 +1,2 @@
+document.addEventListener("click",(function(t){t.target.classList.contains("hamburger-toggle")&&t.target.children[0].classList.toggle("active")}));
+//# sourceMappingURL=form.b1603c8d.js.map
