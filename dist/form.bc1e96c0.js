24191dd6a0dfbec5f72b65133a3dfb45e917a8e6
Maider777 <maidertxu13@gmail.com>
2021-10-18 13:25:49 +0200

    modificado2

---
+++ dist/form.bc1e96c0.js
@@ -0,0 +1,10 @@
+// (c) 2020-2021 Written by Simon Köhler in Panama
+// github.com/koehlersimon
+// simon-koehler.com
+// No JavaScript needed for this example anymore!
+document.addEventListener('click', function(e) {
+    // Hamburger menu
+    if (e.target.classList.contains('hamburger-toggle')) e.target.children[0].classList.toggle('active');
+});
+
+//# sourceMappingURL=form.bc1e96c0.js.map
