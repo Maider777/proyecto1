fcb293d5be5573993a1e4a6a5eec594d649172a1
Maider777 <maidertxu13@gmail.com>
2021-10-18 13:05:05 +0200

    modificado

--- dist/form.b1603c8d.js
+++
@@ -1,2 +0,0 @@
-document.addEventListener("click",(function(t){t.target.classList.contains("hamburger-toggle")&&t.target.children[0].classList.toggle("active")}));
-//# sourceMappingURL=form.b1603c8d.js.map
